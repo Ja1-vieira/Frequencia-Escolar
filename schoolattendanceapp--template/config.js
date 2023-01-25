4a5b44e1b7ac7b12b09b0fd4cfa4470f83ca00bf
Ja1-vieira <91163288+Ja1-vieira@users.noreply.github.com>
2023-01-25 11:27:08 -0300

    Add files via upload

---
+++ schoolattendanceapp--template/config.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDGiZUVugYLxS6jnDtrMUiR1dl7mDe7h7k",
+  authDomain: "frequencia-escolar-bb199.firebaseapp.com",
+  projectId: "frequencia-escolar-bb199",
+  storageBucket: "frequencia-escolar-bb199.appspot.com",
+  messagingSenderId: "15459603659",
+  appId: "1:15459603659:web:fd3a764148ebf6f9f0374e"
+};
+
+  export default firebase.database()
+ 
+
+  
